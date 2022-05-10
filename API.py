ddc0441fd586f392d55b23153fd687d7b2f4720f
shernandezf <s.hernandezf@uniandes.edu.co>
2022-05-09 19:53:48 -0500

    api funcional

--- API.py
+++ API.py
@@ -3,8 +3,19 @@ from pydantic import BaseModel
 from fastapi import FastAPI
 import pandas as pd
 from joblib import load
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+origins=[
+    'http://127.0.0.1:5500',
+    'https://tallerbiapp.herokuapp.com'
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials= True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 class DataModelapp(BaseModel):
     Limpiostudycondition: str
 @app.get("/")
